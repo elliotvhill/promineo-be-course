4e05e51185d0acf6eabe3f0db4dafd62ece1e106
Elliot <ellthrowaway2@icloud.com>
2024-12-19 23:10:10 -0500

    trying a different approach

--- eclipse-workspace/Labs/src/week06/GameBoard.java
+++ eclipse-workspace/Labs/src/week06/GameBoard.java
@@ -2,52 +2,23 @@ package week06;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class GameBoard {
 
-	static GameTile[] gameBoard = new GameTile[9];
-
+	// 9 gameboard tiles/slots
+	List<Map<String, Boolean>> gameTiles = new ArrayList<Map<String, Boolean>>();
+	
 	// Default constructor
-	public void setGameBoard(GameTile tile) {
-		// Set each tile value to 1-9 and isOccupied to false
+	public GameBoard() {}
+
+	
+	// Methods
+	private void generateGameBoard() {
 		for (int i = 0; i < 9; i++) {
-			gameBoard[i] = new GameTile(i + 1, false, i);
-			tile.tileValue = null;
+			// Generate a new tile with a value 1-9 (as a string) and isOccupied false
+			GameTile tile = new GameTile(Integer.toString(i + 1), false);
 		}
-	};
-	
-	// Get tile
-	public GameTile findTile(int userInput) {
-		GameTile foundTile = gameBoard[userInput - 1];
-		return foundTile;
 	}
 	
-	// Update game tile
-	public void updateGameTile(int index, String xOrO) {
-		gameBoard[index].tileValue = xOrO;
-		gameBoard[index].isOccupied = true;
-	}
-
-	// Print game board to console
-	public void printBoard() {
-		System.out.println("+---+---+---+");
-		for (int i = 0; i < 9; i++) {
-			System.out.print(String.format("| %s ", gameBoard[i].tileValue));
-		}
-		System.out.println("+---+---+---+");
-	}
-	
-	// print NEW game board to console
-//	public void printNewBoard() {
-//		System.out.println("+---+---+---+");
-//		for (int i = 0; i < gameBoard.length - 1; i+=3) {
-//			System.out.print(String.format(
-//					"| %s | %s | %s |\n+---+---+---+\n", i + 1, i + 2, i + 3));
-//		}
-//	}
-
-	public void getGameResult() {
-
-	}
-
 }
--- eclipse-workspace/Labs/src/week06/GameTile.java
+++ eclipse-workspace/Labs/src/week06/GameTile.java
@@ -1,35 +1,13 @@
 package week06;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class GameTile {
 
-	// "X" tile = -1
-	// "O" tile = 0
-
-	static Map<Integer, HashMap<String, Boolean>> tiles = new HashMap<Integer, HashMap<String, Boolean>>();
 	String tileValue;
 	boolean isOccupied;
-	int tileNumber;
 
 	// Default constructor
-	public GameTile(int tileValue, boolean isOccupied, int tileNumber) {
-		this.tileValue = Integer.toString(tileValue);
-		this.isOccupied = isOccupied;
-		this.tileNumber = tileNumber;
-	}
-
-	boolean getIsOccupied() {
-		return isOccupied;
-	}
-
-	void setIsOccupied(boolean isOccupied) {
+	public GameTile(String tileValue, boolean isOccupied) {
+		this.tileValue = tileValue;
 		this.isOccupied = isOccupied;
 	}
-
-	String setTileValue(GameTile tile) {
-		return this.tileValue = tile.tileValue;
-	};
-
 }
