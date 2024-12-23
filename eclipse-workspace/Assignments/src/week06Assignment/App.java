3145fc246d914d1a592e9e2599b2ebfccd96ba1e
Elliot <ellthrowaway2@icloud.com>
2024-12-23 11:11:29 -0500

    feat: generate new card deck method creates 13 cards of each value and suit, 52 total

--- eclipse-workspace/Assignments/src/week06Assignment/App.java
+++ eclipse-workspace/Assignments/src/week06Assignment/App.java
@@ -3,14 +3,16 @@ package week06Assignment;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Card testCard = new Card("Two", "Hearts", 2);
 		testCard.describe();
-		
+
 		Player player1 = new Player("Geralt", 0);
 		player1.describe();
-		
+
+		Deck deck = new Deck();
+		deck.generateNewDeck();
+
 	}
 
 }
--- eclipse-workspace/Assignments/src/week06Assignment/Deck.java
+++ eclipse-workspace/Assignments/src/week06Assignment/Deck.java
@@ -1,11 +1,31 @@
 package week06Assignment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
 
-	private List<Card> cards;
-	
-	
-	
+	public List<Card> cards = new ArrayList<Card>();
+	private String[] cardNames = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+			"Queen", "King", "Ace" };
+	private String[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
+
+	public List<Card> generateNewDeck() {
+		// Generate cards of each suit
+		for (String suit : suits) {
+			// Generate cards of each name and value
+			for (int i = 0; i < 13; i++) {
+				Card cardToAddToDeck = new Card(cardNames[i], suit, i + 2);
+				cards.add(cardToAddToDeck);
+				System.out.println(String.format("Card added: %s of %s, value: %d", cardToAddToDeck.getName(),
+						cardToAddToDeck.getSuit(), cardToAddToDeck.getValue()));
+			}
+		}
+		System.out.println("Deck size: " + cards.size());
+		return cards;
+	}
+
+	// shuffle method
+	// draw method
+
 }
