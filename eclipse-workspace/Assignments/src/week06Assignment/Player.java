c3758f224cd3712aca9178ea00d1beddff2f23d9
Elliot <ellthrowaway2@icloud.com>
2024-12-29 16:05:19 -0500

    week 6: fix - formatting

--- eclipse-workspace/Assignments/src/week06Assignment/Player.java
+++ eclipse-workspace/Assignments/src/week06Assignment/Player.java
@@ -8,43 +8,42 @@ public class Player {
 	private List<Card> hand = new ArrayList<Card>();
 	private int score;
 	private String name;
-	
+
 	// Initial constructor before player has drawn any cards
 	public Player(String name, int score) {
 		this.name = name;
 		this.score = 0;
 	}
-	
+
 	// Constructor -- initializes score to 0 to start
 	public Player(List<Card> hand, String name) {
 		this.hand = hand;
 		this.score = 0;
 		this.name = name;
 	}
-	
+
 	// Getters & Setters
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String playerName) {
 		name = playerName;
 	}
-	
+
 	public int getScore() {
 		return score;
 	}
-	
+
 	public List<Card> getHand() {
 		return hand;
 	}
-	
+
 	public void setHand(List<Card> playerHand) {
 		hand = playerHand;
 	}
-	
-	
+
 	// Describe method
 	public void describe() {
 		// If player has been dealt cards
@@ -58,20 +57,20 @@ public class Player {
 			System.out.println(String.format("\nPlayer: %s\n Score: %d", name, score));
 		}
 	}
-	
+
 	// Flip method -- return and remove first card in player's hand
 	public Card flip() {
 		return hand.removeFirst();
 	}
-	
+
 	// Draw method -- draw card from deck and add to player's hand
 	public void draw(Deck deck) {
 		hand.add(deck.draw());
 	}
-	
+
 	// Increment score method
 	public int incrementScore() {
 		return score++;
 	}
-	
+
 }
