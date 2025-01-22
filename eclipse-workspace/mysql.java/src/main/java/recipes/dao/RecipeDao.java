80933c4763dbfcdd78eac2ab953eef3812f590c8
Elliot <ellthrowaway2@icloud.com>
2025-01-22 13:35:07 -0500

    created recipe DAO class with empty executeBatch method to start

---
+++ eclipse-workspace/mysql.java/src/main/java/recipes/dao/RecipeDao.java
@@ -0,0 +1,13 @@
+package recipes.dao;
+
+import java.util.List;
+
+import provided.util.DaoBase;
+
+public class RecipeDao extends DaoBase {
+
+	public void executeBatch(List<String> sqlBatch) {
+		
+	}
+	
+}
