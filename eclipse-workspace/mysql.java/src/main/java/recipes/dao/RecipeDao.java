26c3626fb277b650ff9c03e79b34e8932565a94a
Elliot <ellthrowaway2@icloud.com>
2025-01-24 12:52:34 -0500

    feat: week 9 recipes- built method to insert a new recipe into database using a prepared statement

--- eclipse-workspace/mysql.java/src/main/java/recipes/dao/RecipeDao.java
+++ eclipse-workspace/mysql.java/src/main/java/recipes/dao/RecipeDao.java
@@ -1,24 +1,74 @@
 package recipes.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalTime;
 import java.util.List;
 
 import provided.util.DaoBase;
+import recipes.entity.Recipe;
 import recipes.exception.DbException;
 
 public class RecipeDao extends DaoBase {
 
+	private static final String CATEGORY_TABLE = "category";
+	private static final String INGREDIENT_TABLE = "ingredient";
+	private static final String RECIPE_TABLE = "recipe";
+	private static final String RECIPE_CATEGORY_TABLE = "recipe_category";
+	private static final String STEP_TABLE = "step";
+	private static final String UNIT_TABLE = "unit";
+
+	public Recipe insertRecipe(Recipe recipe) {
+		// @formatter:off
+		String sql = ""
+				+ "INSERT INTO " + RECIPE_TABLE + " "
+				+ "(recipe_name, notes, num_servings, prep_time, cook_time) "
+				+ "VALUES "
+				+ "(?, ?, ?, ?, ?)"; // '?' are placeholders 
+		// @formatter:on
+
+		// Obtain connection
+		try (Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+
+			// Create a PreparedStatement
+			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+				// Set the parameters -- reminder: params are 1-based
+				setParameter(stmt, 1, recipe.getRecipeName(), String.class);
+				setParameter(stmt, 2, recipe.getNotes(), String.class);
+				setParameter(stmt, 3, recipe.getNumServings(), Integer.class);
+				setParameter(stmt, 4, recipe.getPrepTime(), LocalTime.class);
+				setParameter(stmt, 5, recipe.getCookTime(), LocalTime.class);
+
+				// Call the executeUpdate method on the Statement to do the INSERT
+				stmt.executeUpdate();
+				Integer recipeId = getLastInsertId(conn, RECIPE_TABLE);
+
+				commitTransaction(conn);
+
+				recipe.setRecipeId(recipeId);
+				return recipe;
+			} catch (Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+
+	}
+
 	public void executeBatch(List<String> sqlBatch) {
 		try (Connection conn = DbConnection.getConnection()) {
 			startTransaction(conn);
-			
+
 			try (Statement stmt = conn.createStatement()) {
 				for (String sql : sqlBatch) {
 					stmt.addBatch(sql);
 				}
-				
+
 				stmt.executeBatch();
 				commitTransaction(conn);
 			} catch (Exception e) {
