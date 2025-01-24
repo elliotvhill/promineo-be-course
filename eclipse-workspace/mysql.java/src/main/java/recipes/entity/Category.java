935c972a62e5d7792b6f0347abc292d37e40ed08
Elliot <ellthrowaway2@icloud.com>
2025-01-24 12:03:49 -0500

    feat: week 9 recipes- added getters and setters, toString method to Category class

--- eclipse-workspace/mysql.java/src/main/java/recipes/entity/Category.java
+++ eclipse-workspace/mysql.java/src/main/java/recipes/entity/Category.java
@@ -3,4 +3,38 @@ package recipes.entity;
 public class Category {
 	private Integer categoryId;
 	private String categoryName;
+
+	/**
+	 * @return the categoryId
+	 */
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	/**
+	 * @param categoryId the categoryId to set
+	 */
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	/**
+	 * @return the categoryName
+	 */
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	/**
+	 * @param categoryName the categoryName to set
+	 */
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "ID=" + categoryId + ", categoryName=" + categoryName;
+	}
+
 }
