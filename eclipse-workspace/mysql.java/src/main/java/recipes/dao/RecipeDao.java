14f6538bb44236aec306a1073d5b987875187962
Elliot <ellthrowaway2@icloud.com>
2025-01-28 17:48:42 -0500

    feat: week 10- added fetch ingredients method that gets list of ingredients and units

--- eclipse-workspace/mysql.java/src/main/java/recipes/dao/RecipeDao.java
+++ eclipse-workspace/mysql.java/src/main/java/recipes/dao/RecipeDao.java
@@ -6,11 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalTime;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import provided.util.DaoBase;
+import recipes.entity.Ingredient;
 import recipes.entity.Recipe;
+import recipes.entity.Unit;
 import recipes.exception.DbException;
 
 public class RecipeDao extends DaoBase {
@@ -64,41 +69,113 @@ public class RecipeDao extends DaoBase {
 
 	public List<Recipe> fetchAllRecipes() {
 		String sql = "SELECT * FROM " + RECIPE_TABLE + " ORDER BY recipe_name";
-		
+
 		try (Connection conn = DbConnection.getConnection()) {
 			startTransaction(conn);
-			
+
 			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 				try (ResultSet rs = stmt.executeQuery()) {
 					List<Recipe> recipes = new LinkedList<>();
-					
+
 					while (rs.next()) {
 						recipes.add(extract(rs, Recipe.class));
 					}
-					
+
 					return recipes;
 				}
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				rollbackTransaction(conn);
 				throw new DbException(e);
 			}
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e);
 		}
 	}
 
-	
+	public Optional<Recipe> fetchRecipeById(Integer recipeId) {
+		String sql = "SELECT * FROM " + RECIPE_TABLE + " WHERE recipe_id = ?";
+
+		try (Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+
+			try {
+				Recipe recipe = null;
+
+				try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+					setParameter(stmt, 1, recipeId, Integer.class);
+
+					try (ResultSet rs = stmt.executeQuery()) {
+						if (rs.next()) {
+							recipe = extract(rs, Recipe.class);
+						}
+					}
+				}
+				// If recipe is found i.e. not null
+				if (Objects.nonNull(recipe)) {
+
+					// Get ingredients
+					recipe.getIngredients().addAll(fetchRecipeIngredients(conn, recipeId));
+
+					// Get steps
+					recipe.getSteps().addAll(fetchRecipeSteps(conn, recipeId));
+
+					// Get categories
+					recipe.getCategories().addAll(fetchRecipeCategories(conn, recipeId));
+				}
+			} catch (Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	}
+
+	private List<Ingredient> fetchRecipeIngredients(Connection conn, Integer recipeId) throws SQLException {
+		// @formatter:off
+		String sql = ""
+				+ "SELECT i.*, u.unit_name_singular, u.unit_name_plural "
+				+ "FROM " + INGREDIENT_TABLE + " i"
+				+ " LEFT JOIN " + UNIT_TABLE + " u USING (unit_id) "
+				+ "WHERE recipe_id = ? "
+				+ "ORDER BY i.ingredient_order";
+		// @formatter:on
+
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			setParameter(stmt, 1, recipeId, Integer.class);
+
+			try (ResultSet rs = stmt.executeQuery()) {
+				List<Ingredient> ingredients = new LinkedList<>();
+
+				while (rs.next()) {
+					// Get ingredient
+					Ingredient ingredient = extract(rs, Ingredient.class);
+					// Get ingredient unit
+					Unit unit = extract(rs, Unit.class);
+
+					// Add the unit to the ingredient
+					ingredient.setUnit(unit);
+					// Add the ingredient with the unit to the list
+					ingredients.add(ingredient);
+				}
+
+				return ingredients;
+			}
+		}
+
+	}
+
 	public void executeBatch(List<String> sqlBatch) {
 		try (Connection conn = DbConnection.getConnection()) {
 			startTransaction(conn);
-			
+
 			try (Statement stmt = conn.createStatement()) {
 				for (String sql : sqlBatch) {
 					stmt.addBatch(sql);
 				}
-				
+
 				stmt.executeBatch();
 				commitTransaction(conn);
 			} catch (Exception e) {
