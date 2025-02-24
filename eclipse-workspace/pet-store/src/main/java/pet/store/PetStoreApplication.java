e0ca076e8dbb834ab42b05f22a8cef59ae041bc7
Elliot <ellthrowaway2@icloud.com>
2025-02-24 13:17:29 -0500

    created pet store application class and set up as Spring Boot app

---
+++ eclipse-workspace/pet-store/src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
