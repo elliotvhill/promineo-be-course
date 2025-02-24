8a04ff610769075de8183e199a4fee3620e67a00
Elliot <ellthrowaway2@icloud.com>
2025-02-24 13:39:21 -0500

    week 13- cleaned up

--- eclipse-workspace/pet-store/src/main/java/pet/store/entity/PetStore.java
+++ eclipse-workspace/pet-store/src/main/java/pet/store/entity/PetStore.java
@@ -32,12 +32,14 @@ public class PetStore {
 	private Long petStoreZip;
 	private String petStorePhone;
 
-	@EqualsAndHashCode.Exclude
-	@ToString.Exclude
 	@ManyToMany(cascade = CascadeType.PERSIST)
 	@JoinTable(name = "pet_store_customer", joinColumns = @JoinColumn(name = "pet_store_id"), inverseJoinColumns = @JoinColumn(name = "customer_id"))
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
 	private Set<Customer> customers = new HashSet<>();
 
 	@OneToMany(mappedBy = "petStore", cascade = CascadeType.ALL, orphanRemoval = true)
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
 	private Set<Employee> employees = new HashSet<>();
 }
