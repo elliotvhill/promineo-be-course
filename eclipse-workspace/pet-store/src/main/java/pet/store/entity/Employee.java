4af00d2314beb9067040f8b8c857821b4251a4f4
Elliot <ellthrowaway2@icloud.com>
2025-02-24 13:39:05 -0500

    week 13- fixed error, unnecessary pet store id variable in employee class

--- eclipse-workspace/pet-store/src/main/java/pet/store/entity/Employee.java
+++ eclipse-workspace/pet-store/src/main/java/pet/store/entity/Employee.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Data
@@ -18,12 +20,13 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long employeeId;
-	private Long petStoreId;
 	private String employeeFirstName;
 	private String employeeLastName;
 	private String employeePhone;
 	private String employeeJobTitle;
 
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "pet_store_id")
 	private PetStore petStore;
