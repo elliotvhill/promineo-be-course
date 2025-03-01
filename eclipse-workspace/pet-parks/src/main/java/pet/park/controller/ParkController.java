e33cadfa4fca1581d009ecafcf014a4e51efa0a5
Elliot <ellthrowaway2@icloud.com>
2025-03-01 15:47:47 -0500

    week 14- added methods to get all contributors and get contributor by id

--- eclipse-workspace/pet-parks/src/main/java/pet/park/controller/ParkController.java
+++ eclipse-workspace/pet-parks/src/main/java/pet/park/controller/ParkController.java
@@ -3,8 +3,12 @@
  */
 package pet.park.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +43,20 @@ public class ParkController {
 		return parkService.saveContributor(contributorData);
 
 	}
+	
+	@GetMapping("/contributor")
+	public List<ContributorData> retrieveAllContributors() {
+		// Log the GET request
+		log.info("Retrieve all contributors called.");
+		// Call ParkService and return the results of the method
+		return parkService.retrieveAllContributors();
+	}
+	
+	@GetMapping("/contributor/{contributorId}")
+	public ContributorData retrieveContributorById(@PathVariable Long contributorId) { // PathVariable tells Spring we expect the id from the url and to pass it to the method as contributorId
+		log.info("Retrieving contributor with ID={}", contributorId);
+		return parkService.retrieveContributorById(contributorId);
+		
+	}
+	
 }
--- eclipse-workspace/pet-parks/src/main/java/pet/park/service/ParkService.java
+++ eclipse-workspace/pet-parks/src/main/java/pet/park/service/ParkService.java
@@ -3,6 +3,8 @@
  */
 package pet.park.service;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
@@ -14,6 +16,7 @@ import pet.park.controller.model.ContributorData;
 import pet.park.dao.ContributorDao;
 import pet.park.entity.Contributor;
 
+@SuppressWarnings("unused")
 @Service
 public class ParkService {
 
@@ -28,10 +31,10 @@ public class ParkService {
 	public ContributorData saveContributor(ContributorData contributorData) {
 		Long contributorId = contributorData.getContributorId();
 		Contributor contributor = findOrCreateContributor(contributorId);
-		
+
 		setFieldsInContributor(contributor, contributorData);
 		return new ContributorData(contributorDao.save(contributor));
-		
+
 	}
 
 	/**
@@ -67,6 +70,42 @@ public class ParkService {
 	private Contributor findContributorById(Long contributorId) {
 		return contributorDao.findById(contributorId).orElseThrow(
 				() -> new NoSuchElementException("Contributor with ID=" + contributorId + " was not found."));
+	}
+
+	/**
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public List<ContributorData> retrieveAllContributors() {
+		// Method 1:
+//		List<Contributor> contributors = contributorDao.findAll();
+//		List<ContributorData> response = new LinkedList<>();
+//		
+//		// Turn the list of Contributor entities into a list of ContributorData
+//		for (Contributor contributor : contributors) {
+//			response.add(new ContributorData(contributor));
+//		}
+//		
+//		return response;
+
+		// Method 2 - using Strings: // new since Java 1.8
+
+		// @formatter:off
+		return contributorDao.findAll()
+			.stream()
+//			.map(cont -> new ContributorData(cont)) // this Lambda can be simplified by using a method reference
+			.map(ContributorData::new) // method reference
+			.toList();
+		// @formatter:on
+	}
+
+	/**
+	 * @param contributorId
+	 * @return
+	 */
+	public ContributorData retrieveContributorById(Long contributorId) {
+		Contributor contributor = findContributorById(contributorId);
+		return new ContributorData(contributor);
 	}
 
 }
