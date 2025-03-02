703584e0efa6831fe47896aac1a286f0cda39236
Elliot <ellthrowaway2@icloud.com>
2025-03-01 21:35:01 -0500

    week 14- coding assignment progress; building find or create pet store method

---
+++ eclipse-workspace/pet-store/src/main/java/pet/store/service/PetStoreService.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package pet.store.service;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pet.store.controller.model.PetStoreData;
+import pet.store.dao.PetStoreDao;
+import pet.store.entity.PetStore;
+
+@Service
+public class PetStoreService {
+	@Autowired
+	private PetStoreDao petStoreDao;
+
+	@Transactional(readOnly = false)
+	public PetStoreData savePetStore(PetStoreData petStoreData) {
+		PetStore petStore = findOrCreatePetStore(petStoreData.getPetStoreId());
+		copyPetStoreFields(petStore, petStoreData);
+		
+		PetStore dbPetStore = petStoreDao.save(petStore);
+		return new PetStoreData(dbPetStore);
+	}
+
+	private void copyPetStoreFields(PetStore petStore, PetStoreData petStoreData) {
+		petStore.setPetStoreId(petStoreData.getPetStoreId());
+		petStore.setPetStoreName(petStoreData.getPetStoreName());
+		petStore.setPetStoreAddress(petStoreData.getPetStoreAddress());
+		petStore.setPetStoreCity(petStoreData.getPetStoreCity());
+		petStore.setPetStoreState(petStoreData.getPetStoreState());
+		petStore.setPetStoreZip(petStoreData.getPetStoreZip());
+		petStore.setPetStorePhone(petStoreData.getPetStorePhone());
+	}
+
+	private PetStore findOrCreatePetStore(Long petStoreId) {
+		PetStore petStore;
+
+		// create a new PetStore object if petStoreId is null
+		if (Objects.isNull(petStoreId)) {
+			// create a new PetStore object
+			petStore = new PetStore();
+		} else {
+			// Find the existing pet store by ID
+			petStore = findPetStoreById(petStoreId);
+		}
+
+		return petStore;
+	}
+
+	private PetStore findPetStoreById(Long petStoreId) {
+		return petStoreDao.findById(petStoreId).orElseThrow(() -> new NoSuchElementException("Pet store with ID=" + petStoreId + " does not exist."));
+	}
+}
