567105f6443a56a39ef89c25c5cc94bcabe00328
Elliot <ellthrowaway2@icloud.com>
2025-03-09 15:22:43 -0400

    week 15- created method to add a customer to a pet store

--- eclipse-workspace/pet-store/src/main/java/pet/store/controller/PetStoreController.java
+++ eclipse-workspace/pet-store/src/main/java/pet/store/controller/PetStoreController.java
@@ -5,7 +5,6 @@ package pet.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 import pet.store.controller.model.PetStoreData;
+import pet.store.controller.model.PetStoreData.PetStoreCustomer;
 import pet.store.controller.model.PetStoreData.PetStoreEmployee;
 import pet.store.service.PetStoreService;
 
@@ -40,13 +40,22 @@ public class PetStoreController {
 		log.info("Attempting to update pet store {}", petStoreData);
 		return petStoreService.savePetStore(petStoreData);
 	}
-	
+
 	// Add an employee to the employee table
 	@PostMapping("/{petStoreId}/employee")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public PetStoreEmployee addEmployee(@PathVariable Long petStoreId, @RequestBody PetStoreEmployee petStoreEmployee) {
 		log.info("Adding employee {} to pet store ID={}", petStoreEmployee, petStoreId);
-		
+
 		return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
 	}
+
+	// Add a Pet Store Customer
+	@PostMapping("/{petStoreId}/customer")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public PetStoreCustomer addCustomer(@PathVariable Long petStoreId, @RequestBody PetStoreCustomer petStoreCustomer) {
+		log.info("Adding customer={} to pet store ID={}", petStoreCustomer, petStoreId);
+		return petStoreService.saveCustomer(petStoreId, petStoreCustomer);
+	}
+
 }
