a56eb987a0fb9478692048a948b7563a585fa30b
Elliot <ellthrowaway2@icloud.com>
2025-03-12 18:52:45 -0400

    week 16- added helper methods for testing update location

--- eclipse-workspace/dog-rescue/src/test/java/dog/rescue/controller/RescueServiceTestSupport.java
+++ eclipse-workspace/dog-rescue/src/test/java/dog/rescue/controller/RescueServiceTestSupport.java
@@ -34,6 +34,17 @@ public class RescueServiceTestSupport {
 			"32556",
 			"(850) 204-9485"
 	);
+	
+	private LocationData updateAddress1 = new LocationData(
+			1L,
+			"Glenlake Dog Rescue Society",
+			"8 East Glenlake Drive",
+			"Wadsworth",
+			"Ohio",
+			"83241",
+			"(330) 336-2105"
+			);
+	
 	// @formatter:on
 
 	@Autowired
@@ -103,5 +114,20 @@ public class RescueServiceTestSupport {
 		 */
 		data.sort((loc1, loc2) -> (int) (loc1.getLocationId() - loc2.getLocationId()));
 		return data;
+	}
+	
+	/**
+	 * @param expected
+	 * @return
+	 */
+	protected LocationData updateLocation(LocationData locationData) {
+		return rescueController.updateLocation(locationData.getLocationId(), locationData); // pass in the ID and the response body i.e. locationData
+	}
+
+	/**
+	 * @return
+	 */
+	protected LocationData buildUpdateLocation() {
+		return updateAddress1;
 	}
 }
