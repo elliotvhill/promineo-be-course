9b79bad3b7ffd039e2ca0a3be00a0e6d05b2e5e1
Elliot <ellthrowaway2@icloud.com>
2025-03-12 18:41:31 -0400

    week 16- created method to update a location by location ID

--- eclipse-workspace/dog-rescue/src/main/java/dog/rescue/controller/RescueController.java
+++ eclipse-workspace/dog-rescue/src/main/java/dog/rescue/controller/RescueController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,20 +23,28 @@ import lombok.extern.slf4j.Slf4j;
 public class RescueController {
 	@Autowired
 	private RescueService rescueService;
-	
+
 	@PostMapping("/location")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public LocationData createLocation(@RequestBody LocationData locationData) {
 		log.info("Creating location {}", locationData);
 		return rescueService.saveLocation(locationData);
 	}
-	
+
+	@PutMapping("/location/{locationId}")
+	public LocationData updateLocation(@PathVariable Long locationId, @RequestBody LocationData locationData) {
+		// Add (set) location ID from URI to the LocationData object
+		locationData.setLocationId(locationId);
+		log.info("Updating location {}", locationData);
+		return rescueService.saveLocation(locationData);
+	}
+
 	@GetMapping("/location/{locationId}")
 	public LocationData retrieveLocation(@PathVariable Long locationId) {
 		log.info("Retrieving location with ID={}", locationId);
 		return rescueService.retrieveLocationById(locationId);
 	}
-	
+
 	@GetMapping("/location")
 	public List<LocationData> retrieveAllLocations() {
 		log.info("Retrieving all locations");
