79970a56966933f07812cdb3d102c240ec9a2f35
Elliot <ellthrowaway2@icloud.com>
2025-03-12 17:46:34 -0400

    week 16- created retrieve all locations service method; retrieve as Location entities and then convert to LocationData entities

--- eclipse-workspace/dog-rescue/src/main/java/dog/rescue/service/RescueService.java
+++ eclipse-workspace/dog-rescue/src/main/java/dog/rescue/service/RescueService.java
@@ -1,5 +1,7 @@
 package dog.rescue.service;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,8 +47,33 @@ public class RescueService {
 	 */
 	private Location findLocationById(Long locationId) {
 		return locationDao.findById(locationId)
-				.orElseThrow(() -> new NoSuchElementException(
-						"Location with ID=" + locationId + " was not found."));
+				.orElseThrow(() -> new NoSuchElementException("Location with ID=" + locationId + " was not found."));
+	}
+
+	@Transactional(readOnly = true)
+	public List<LocationData> retrieveAllLocations() {
+		// One approach: use enhanced for loop
+//		List<Location> locationEntities = locationDao.findAll();
+//		List<LocationData> locationDtos = new LinkedList<>();
+//		
+//		// loop to convert Location entities into LocationData objects
+//		for (Location location : locationEntities) {
+//			LocationData locationData = new LocationData(location);
+//			locationDtos.add(locationData);
+//		}
+//		
+//		return locationDtos;
+
+		// Alternate approach: Use Streams and Lambda expression
+		// @formatter:off
+		return locationDao.findAll()
+				// turn into a Stream of Location entities
+				.stream()
+				// use map to turn it into a Stream of LocationData objects
+				.map(loc -> new LocationData(loc)) // can be further simplified using a method reference operator ("::") to just .map(LocationData::new)
+				// return it as a List
+				.toList();
+		// @formatter:on
 	}
 
 }
