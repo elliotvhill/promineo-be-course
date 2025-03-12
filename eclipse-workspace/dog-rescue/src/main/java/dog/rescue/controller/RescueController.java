8e3ef5ce249edd46902dc1239f8a99903d829e67
Elliot <ellthrowaway2@icloud.com>
2025-03-12 19:15:18 -0400

    week 16- created delete location method

--- eclipse-workspace/dog-rescue/src/main/java/dog/rescue/controller/RescueController.java
+++ eclipse-workspace/dog-rescue/src/main/java/dog/rescue/controller/RescueController.java
@@ -1,9 +1,11 @@
 package dog.rescue.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +51,13 @@ public class RescueController {
 	public List<LocationData> retrieveAllLocations() {
 		log.info("Retrieving all locations");
 		return rescueService.retrieveAllLocations();
+	}
+	
+	@DeleteMapping("/location/{locationId}")
+	public Map<String, String> deleteLocation(@PathVariable Long locationId) {
+		log.info("Deleting location with ID=" + locationId + ".");
+		rescueService.deleteLocation(locationId);
+		
+		return Map.of("message", "Location with ID=" + locationId + " was deleted.");
 	}
 }
